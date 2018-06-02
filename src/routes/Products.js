e23cf8668d1d311c073c360ce15704e7b7b4df5f
Hiu-Fung Hau <dafungz@htomail.com>
2018-06-02 09:42:34 +0200

    Show products and images in FlatList

--- src/routes/Products.js
+++ src/routes/Products.js
@@ -1,9 +1,39 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, FlatList, Image } from 'react-native';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import { BASE_URL } from '../constants';
 
-export default ({ history }) => (
-    <View>
-        <Text style={{ marginTop: 50 }}>Products</Text>
-        <Button title="Create Product" onPress={()=> history.push('/new-product')}></Button>
-    </View>
-);
+const Products = ({ data: { products }, loading, history }) =>
+    ((loading || !products) ? null : (
+        <View>
+            <Text style={{ marginTop: 50 }}>Products</Text>
+            <Button title="Create Product" onPress={()=> history.push('/new-product')}/>
+            <FlatList
+                data={products}
+                keyExtractor={(item) => item.id}
+                renderItem=
+                    {({ item }, i) =>
+                    <View>
+                    <Text>{item.name}</Text>
+                    <Text>{item.price}</Text>
+                    <Text>{item.pictureUrl}</Text>
+                    <Image style={{ height:50, width: 50 }} source={{ uri: `${BASE_URL}/${item.pictureUrl}` }}/>
+                    </View>
+                    }
+            />
+        </View>
+    ));
+
+const productsQuery = gql`
+    {
+        products {
+            id
+            name
+            price
+            pictureUrl
+        }
+    }
+`
+
+export default graphql(productsQuery)(Products);
