acba414adda4d43897c407a5eda94ad428fe42e4
Hiu-Fung Hau <dafungz@htomail.com>
2018-06-12 20:47:06 +0200

    Add searchBar and SortButtons

--- src/routes/Products.js
+++ src/routes/Products.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AsyncStorage, Text, View, Button, FlatList, Image, StyleSheet } from 'react-native';
+import { AsyncStorage, Text, TextInput, View, Button, TouchableOpacity, FlatList, Image, StyleSheet } from 'react-native';
 import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 import { BASE_URL, USER_ID_KEY } from '../constants';
@@ -31,6 +31,25 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'row',
     },
+    searchBar: {
+        margin: 10,
+    },
+    sortRow: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    sortButton: {
+        flex: 1,
+        borderRadius: 4,
+        borderWidth: 0.5,
+        borderColor: '#d6d7da',
+        alignItems: 'center'
+    },
+    buttonText: {
+        fontSize: 20,
+        color: 'gray',
+    }
 });
 
 class Products extends React.Component {
@@ -64,8 +83,17 @@ class Products extends React.Component {
         const { userId } = this.state;
 
         return (
-            <View style={{ marginBottom: 150 }}>
-                <Text style={{ marginTop: 50 }}>Products</Text>
+            <View style={{ marginBottom: 200 }}>
+                <View style={{ marginTop: 30 }}>
+                    <View style={styles.searchBar}>
+                        <TextInput placeholder="search"/>
+                    </View>
+                    <View style={styles.sortRow}>
+                        <TouchableOpacity style={styles.sortButton} title="Name" onPress={() => 5}><Text style={styles.buttonText}>Name</Text></TouchableOpacity>
+                        <TouchableOpacity style={styles.sortButton} title="Price" onPress={() => 5}><Text style={styles.buttonText}>Price</Text></TouchableOpacity>
+                    </View>
+                </View>
+                <Text style={{ marginTop: 20 }}>Products</Text>
                 <Button title="Create Product" onPress={()=> history.push('/new-product')}/>
                 <FlatList
                     data={products}
